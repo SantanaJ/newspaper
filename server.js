b335c923904a83a0b7f91ecc9d8294fcf0a27730
SantanaJ <jsmnsantana1@gmail.com>
2018-05-30 00:14:46 -0400

    cleaning up

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var path = require("path");
-var request = require ("request");
-const cheerio = require("cheerio");
+// Web Scraper Homework Solution Example
+// (be sure to watch the video to see
+// how to operate the site in the browser)
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
 
-var app = express();
+// Require our dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+// Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
 
+// Instantiate our Express App
+var app = express();
+
+// Require our routes
+var routes = require("./routes");
+
+// Designate our public folder as a static directory
 app.use(express.static("public"));
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-var expresshandle = require("express-handlebars");
-
-app.engine("handlebars", expresshandle({defaultLayout: "main"}));
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require ("./controllers/newspaper_controller.js");
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Have every request go through our route middleware
 app.use(routes);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+// Listen on the port
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+  console.log("Listening on port: " + PORT);
+});
